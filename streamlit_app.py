c65aa518f5c5d59a66d02c9faa9329f610651b3d
joseratts <josearodriguezd@gmail.com>
2022-12-05 11:58:06 +0100

    Update streamlit_app.py
    Added menu options

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,8 @@
 import streamlit
 
 streamlit.title('My parents new healthy diner')
+
+streamlit.header('Breakfast menu')
+streamlit.text('Omega 3 and Blueberry Oatmeal')
+streamlit.text('Kale, spinach & Rocket Smoothie')
+streamlit.text('Hard-Bioled Free-Range Egg')
