9f9b81ba483fa286f1a0d24b94472edf801eed8e
joseratts <josearodriguezd@gmail.com>
2022-12-05 12:00:51 +0100

    Update streamlit_app.py
    Added images to every menu item and also added a new header

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,10 @@ import streamlit
 
 streamlit.title('My parents new healthy diner')
 
-streamlit.header('Breakfast menu')
-streamlit.text('Omega 3 and Blueberry Oatmeal')
-streamlit.text('Kale, spinach & Rocket Smoothie')
-streamlit.text('Hard-Bioled Free-Range Egg')
+streamlit.header('Breakfast Favorites')
+streamlit.text('🥣 Omega 3 and Blueberry Oatmeal')
+streamlit.text('🥗 Kale, spinach & Rocket Smoothie')
+streamlit.text('🐔 Hard-Bioled Free-Range Egg')
+streamlit.text('🥑🍞 Avocado Toast')
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
